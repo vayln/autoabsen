f327ea535751ea6dae6c37408c0e5b28f957ad12
vayln <fiqhqy@gmail.com>
2025-07-23 02:02:57 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -3,24 +3,24 @@
 MODDIR=${0%/*}
 LOGFILE="/sdcard/service_log.txt"
 
-# Cegah deviceidle agar tidak deep
+
 dumpsys deviceidle disable
 
 export PATH=/system/bin:/system/xbin:/sbin:/vendor/bin:/vendor/xbin:/system/sbin:$PATH
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') [INFO] service.sh dijalankan sebagai: $(id)" >> "$LOGFILE"
 
-# WAKElOCK agar tidak deep sleep
+
 exec 200>/sys/power/wake_lock
 echo "autoabsen_wakelock" >&200
 
-# Jaga layar tetap hidup (opsional, hanya bantu sedikit)
+
 svc power stayon true
 
-# Loop utama
+
 (
 while true; do
-  MIN=$(date +%M | sed 's/^0*//')  # hilangkan leading zero
+  MIN=$(date +%M | sed 's/^0*//') 
   REM=$((MIN % 5))
   logfile="/sdcard/service_log.txt"
 
